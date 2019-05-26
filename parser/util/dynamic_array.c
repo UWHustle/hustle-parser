dd01e39e97fd87ec18ea62adcb09b49446e850c9
gaffneyk <kgaff33@gmail.com>
2019-05-26 11:59:24 -0500

    Fixed bug with dynamic array malloc

--- parser/util/dynamic_array.c
+++ parser/util/dynamic_array.c
@@ -4,7 +4,7 @@ dynamic_array *dynamic_array_alloc() {
     dynamic_array *array = malloc(sizeof(dynamic_array));
     array->max_size = STARTING_SIZE;
     array->size = 0;
-    array->array = malloc(array->max_size);
+    array->array = malloc(sizeof(void *) * array->max_size);
     return array;
 }
 
@@ -17,8 +17,8 @@ void dynamic_array_push_back(dynamic_array *array, void *item) {
     array->array[array->size] = item;
     array->size++;
     if (array->size == array->max_size) {
-        array->array = realloc(array->array, array->max_size * LOAD_FACTOR);
         array->max_size *= LOAD_FACTOR;
+        array->array = realloc(array->array, array->max_size);
     }
 }
 
